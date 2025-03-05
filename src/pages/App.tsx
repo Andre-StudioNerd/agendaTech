c2c08cbbc50d351dca06526a56807886708bb51e
André Luis Fernandes <62942316+Andre-StudioNerd@users.noreply.github.com>
2025-03-05 12:42:04 -0300

    upadate projeto

---
+++ src/pages/App.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Formulario from "../components/Form";
+import Lista from "../components/List";
+import style from "./style.module.scss";
+import Stopwatch from "../components/Stopwatch";
+import { ITarefa } from "../types/tarefa";
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas((tarefasAnteriores) =>
+      tarefasAnteriores.map((tarefa) => ({
+        ...tarefa,
+        selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+      }))
+    );
+  }
+
+  function finalizarTarefa() {
+    if (selecionado) {
+      setSelecionado(undefined);
+      setTarefas((tarefasAnteriores) =>
+        tarefasAnteriores.map((tarefa) => {
+          if (tarefa.id === selecionado.id) {
+            return {
+              ...tarefa,
+              selecionado: false,
+              completado: true,
+            };
+          }
+          return tarefa;
+        })
+      );
+    }
+  }
+
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+      <Stopwatch selecionado={selecionado} finalizarTarefa={finalizarTarefa} />
+    </div>
+  );
+}
+
+export default App;
